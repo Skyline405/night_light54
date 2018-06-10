a4a9d7dfa2f0feab24ced2dec151fb4945d54b6e
Skyline405 <ars.sky@mail.ru>
2018-06-10 16:59:13 +0700

    Fix multiple props (duplicates)

--- cart/views.py
+++ cart/views.py
@@ -21,8 +21,10 @@ def add_to_cart(req, product_id, quantity=1):
     order_item.save()
 
     for prop in props:
-        pp = ProductProp.objects.get(title=prop['name'], product_id=product.id, value=prop['value'])
-        order_item.props.add(pp)
+        pps = list(ProductProp.objects.filter(title=prop['name'], product_id=product.id, value=prop['value']))
+        if len(pps):
+            pp = pps[0]
+            order_item.props.add(pp)
 
     cart = Cart(req)
     cart.add(order_item, product.price, quantity)
